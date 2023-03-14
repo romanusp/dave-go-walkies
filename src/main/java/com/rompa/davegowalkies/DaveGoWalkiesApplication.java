a9d60063bac6ba3b7022112accb967a8ca6eff34
Paul Romanus <paul.romanus@gmail.com>
2023-03-14 15:01:31 +0000

    Initial Commit

---
+++ src/main/java/com/rompa/davegowalkies/DaveGoWalkiesApplication.java
@@ -0,0 +1,13 @@
+package com.rompa.davegowalkies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaveGoWalkiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaveGoWalkiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rompa/davegowalkies/DaveGoWalkiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rompa.davegowalkies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaveGoWalkiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
