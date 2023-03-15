274b2e99efc6fed32a595e305365c667213c5f9e
Paul Romanus <paul.romanus@gmail.com>
2023-03-15 23:18:34 +0000

    after push remote to feat- branch

--- src/main/java/com/rompa/davegowalkies/DaveGoWalkiesApplication.java
+++ src/main/java/com/rompa/davegowalkies/DaveGoWalkiesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DaveGoWalkiesApplication {
 
 	public static void main(String[] args) {
-		// anothes dippy change for staging ra ra!
+		// anothes dippy change for staging ra ra ra!
 		SpringApplication.run(DaveGoWalkiesApplication.class, args);
 	}
 
